dc7fa9da1472015d704761ba19114ac745efe1a6
DIGITALFREDOTECH <162149183+DIGITALFREDOTECH@users.noreply.github.com>
2024-08-19 15:35:29 +0000

    Update auth.js

--- dist/middleware/auth.js
+++ dist/middleware/auth.js
@@ -1,8 +1,8 @@
 import jwt from "jsonwebtoken";
 import User from "../models/User.js";
 export const authMiddleware = async (req, res, next) => {
-    const token = req.header("Authorization")?.replace("Bearer ", "");
-    console.log(req.headers('Authorization'));
+    const token = req.header["Authorization"]?.replace("Bearer ", "");
+    console.log(req.headers['Authorization']);
     if (!token) {
         return res.status(401).send({ error: "Authentication required" });
     }
