9dc55dda02ea725732fd4c43344edb31c4bb9db0
DIGITALFREDOTECH <162149183+DIGITALFREDOTECH@users.noreply.github.com>
2024-08-19 12:32:49 +0100

    initial commit

--- dist/index.js
+++ dist/index.js
@@ -3,7 +3,9 @@ import http from "http";
 import { Server } from "socket.io";
 import Message from "./models/Message.js";
 const server = http.createServer(app);
-const io = new Server(server, { cors: { origin: "*" } });
+const io = new Server(server, {
+  cors: { origin: "https://chat-frontend-ten-zeta.vercel.app" },
+});
 io.on("connection", (socket) => {
   // ...
   socket.on("join_room", (roomId) => {
